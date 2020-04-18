bd031c411d826fd31ce1c54566e2833661dc9c1a
unknown <delliolan1999@gmail.com>
2020-04-18 21:31:25 +0300

    adds slider for changing color level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePaeltte } from './ColorHelpers';
 
 export default class App extends Component {
   render() {
+    console.log(generatePaeltte(seedColors[3]))
     return (
       <div>
-        <Palette {...seedColors[3]} />
+        <Palette palette={generatePaeltte(seedColors[1])} />
       </div>
     )
   }
