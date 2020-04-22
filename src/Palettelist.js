362d5a125bd4e2b5f4892d922369b2ec6b83e258
unknown <delliolan1999@gmail.com>
2020-04-22 17:52:41 +0300

    starts styling mini palette

--- src/Palettelist.js
+++ src/Palettelist.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom"
+import MiniPalette from './MiniPalette'
+
 export default class Palettelist extends Component {
     render() {
         const {palettes}=this.props
         return (
             <div>
                 {palettes.map(palette=>{
-                    return palette.paletteName
+                    return <MiniPalette {...palette} />
                 })}
             </div>
         )
